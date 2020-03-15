96cd30ce54840975688920be213e006ddfef85d2
Bob Gardner <bob.hn.gardner@gmail.com>
2020-03-15 19:36:47 -0400

    Fix insufficient trends by requesting 2 days worth (#20)
    Always request two days worth of Google Trend data. The email will prefer more recent trends over yesterday's. This is implemented by using the `endDateForNextRequest` field on the Google Trends response.

--- app/pocket_cleaner/src/view.rs
+++ app/pocket_cleaner/src/view.rs
@@ -1,15 +1,28 @@
+use std::convert::TryFrom;
+
 use actix_web::{self, web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    error::Result,
+    error::{PocketCleanerError, Result},
     trends::{self, TrendFinder},
 };
+
+#[derive(Deserialize, Default, Clone, Debug)]
+pub struct TrendsGeo(String);
+
+impl TryFrom<TrendsGeo> for trends::Geo {
+    type Error = PocketCleanerError;
+
+    fn try_from(raw: TrendsGeo) -> std::result::Result<Self, Self::Error> {
+        Self::new(raw.0)
+    }
+}
 
 #[derive(Deserialize)]
 pub struct TrendsRequest {
     #[serde(default)]
-    geo: trends::Geo,
+    geo: TrendsGeo,
 }
 
 #[derive(Serialize)]
@@ -19,7 +32,8 @@ struct TrendsResponse {
 
 pub async fn trends_view(query: web::Query<TrendsRequest>) -> Result<impl Responder> {
     let trend_finder = TrendFinder::new();
-    let trends = trend_finder.daily_trends(&query.geo).await?;
+    let geo = trends::Geo::try_from(query.geo.clone())?;
+    let trends = trend_finder.daily_trends(&geo, 1 /*num_days*/).await?;
 
     Ok(HttpResponse::Ok().json(TrendsResponse {
         trends: trends.iter().map(|t| t.name()).collect(),
