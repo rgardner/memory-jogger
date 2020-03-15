edd32aa839ff166cfe8dfe0c17073f18922359a4
Bob Gardner <bob.hn.gardner@gmail.com>
2020-03-15 11:40:52 -0400

    Add Google Trends response deser test

--- app/pocket_cleaner/src/trends.rs
+++ app/pocket_cleaner/src/trends.rs
@@ -79,29 +79,29 @@ impl DailyTrendsRequest {
 }
 
 /// Top-level Google Trends Daily Trends API response.
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, PartialEq, Debug)]
 struct DailyTrendsResponse {
     default: DailyTrendsData,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, PartialEq, Debug)]
 #[serde(rename_all = "camelCase")]
 struct DailyTrendsData {
     trending_searches_days: Vec<TrendingSearchDay>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, PartialEq, Debug)]
 #[serde(rename_all = "camelCase")]
 struct TrendingSearchDay {
     trending_searches: Vec<TrendingSearch>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, PartialEq, Debug)]
 struct TrendingSearch {
     title: TrendingSearchTitle,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, PartialEq, Debug)]
 struct TrendingSearchTitle {
     /// Trending search query.
     query: String,
@@ -164,5 +164,216 @@ mod tests {
         let empty_geo = Geo("".into());
         let res = trend_finder.daily_trends(&empty_geo).await;
         assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_deserialize_trends_response() {
+        let s = r#"{
+            "default": {
+                "trendingSearchesDays": [
+                    {
+                        "date": "20200314",
+                        "formattedDate": "Saturday, March 14, 2020",
+                        "trendingSearches": [
+                            {
+                                "title": {
+                                    "query": "Coronavirus tips",
+                                    "exploreLink": "/trends/explore?q=Coronavirus+tips&date=now+7-d&geo=US"
+                                },
+                                "formattedTraffic": "2M+",
+                                "relatedQueries": [],
+                                "image": {
+                                    "newsUrl": "https://www.npr.org/2020/03/15/815549926/8-tips-to-make-working-from-home-work-for-you",
+                                    "source": "NPR",
+                                    "imageUrl": "https://t2.gstatic.com/images?q=tbn:ANd9GcQOo9muBZru1De7RslRzgz3KTB0JLqIeR9Y3_1gv1HaDQF5NAxiI8vXVgtA_rcrKeTewqM0lyDR"
+                                },
+                                "articles": [
+                                    {
+                                        "title": "8 Tips To Make Working From Home, Work For You",
+                                        "timeAgo": "4h ago",
+                                        "source": "NPR",
+                                        "image": {
+                                            "newsUrl": "https://www.npr.org/2020/03/15/815549926/8-tips-to-make-working-from-home-work-for-you",
+                                            "source": "NPR",
+                                            "imageUrl": "https://t2.gstatic.com/images?q=tbn:ANd9GcQOo9muBZru1De7RslRzgz3KTB0JLqIeR9Y3_1gv1HaDQF5NAxiI8vXVgtA_rcrKeTewqM0lyDR"
+                                        },
+                                        "url": "https://www.npr.org/2020/03/15/815549926/8-tips-to-make-working-from-home-work-for-you",
+                                        "snippet": "Millions of people are trying to work from home because of coronavirus. Life Kit wants to help WFH work for you, especially if you&#39;re doing so for the first time."
+                                    },
+                                    {
+                                        "title": "Home school teacher offers parents tips amid closings due to ...",
+                                        "timeAgo": "15h ago",
+                                        "source": "FOX 5 Atlanta",
+                                        "image": {
+                                            "newsUrl": "https://www.fox5atlanta.com/news/home-school-teacher-offers-parents-tips-amid-closings-due-to-coronavirus",
+                                            "source": "FOX 5 Atlanta",
+                                            "imageUrl": "https://t3.gstatic.com/images?q=tbn:ANd9GcRhydo2GMnZx78Ofra9idgAMt9aR1q6aJTDaDz1Zt-sTEGjagXZH5-FTnI1kGYPpikgIqW0fSd_"
+                                        },
+                                        "url": "https://www.fox5atlanta.com/news/home-school-teacher-offers-parents-tips-amid-closings-due-to-coronavirus",
+                                        "snippet": "Schools across the country will be closing beginning Monday as fears of the spread of the coronavirus continue. This will create a unique situation where&nbsp;..."
+                                    },
+                                    {
+                                        "title": "Your coronavirus emergency kit: Preparation, symptoms, tips",
+                                        "timeAgo": "6h ago",
+                                        "source": "Aljazeera.com",
+                                        "image": {
+                                            "newsUrl": "https://www.aljazeera.com/news/2020/03/coronavirus-emergency-kit-preparation-symptoms-tips-200314103304717.html",
+                                            "source": "Aljazeera.com",
+                                            "imageUrl": "https://t3.gstatic.com/images?q=tbn:ANd9GcSChU3cpVibmIJsLc7NaKkkot5zOxBmEhzIlw4JhCgXgCRtyj90v_RP9Oe_6EcCapargjj9rCOO"
+                                        },
+                                        "url": "https://www.aljazeera.com/news/2020/03/coronavirus-emergency-kit-preparation-symptoms-tips-200314103304717.html",
+                                        "snippet": "How to prepare or deal with COVID-19 as well as survive a virus-related lockdown."
+                                    },
+                                    {
+                                        "title": "Working from home because of coronavirus? Be careful what you ...",
+                                        "timeAgo": "5h ago",
+                                        "source": "USA TODAY",
+                                        "image": {
+                                            "newsUrl": "https://www.usatoday.com/story/tech/2020/03/15/coronavirus-cyber-safety-tips-working-home/5034081002/",
+                                            "source": "USA TODAY",
+                                            "imageUrl": "https://t3.gstatic.com/images?q=tbn:ANd9GcQQ3PRxORS9J_tIR9BTZUpCDOiNTMDsgADDb8eLrVe0YnBt-Z5fKzOatWqcV8e6wjAVNkwdOf7E"
+                                        },
+                                        "url": "https://www.usatoday.com/story/tech/2020/03/15/coronavirus-cyber-safety-tips-working-home/5034081002/",
+                                        "snippet": "Tips from cybersecurity experts to keep you safe and your computer (and boss) happy while you&#39;re working from home during the coronavirus outbreak."
+                                    }
+                                ],
+                                "shareUrl": "https://trends.google.com/trends/trendingsearches/daily?geo=US&tt=Coronavirus+tips#Coronavirus%20tips"
+                            },
+                            {
+                                "title": {
+                                    "query": "Pi",
+                                    "exploreLink": "/trends/explore?q=Pi&date=now+7-d&geo=US"
+                                },
+                                "formattedTraffic": "500K+",
+                                "relatedQueries": [
+                                    {
+                                        "query": "pi day",
+                                        "exploreLink": "/trends/explore?q=pi+day&date=now+7-d&geo=US"
+                                    },
+                                    {
+                                        "query": "pi day 2020",
+                                        "exploreLink": "/trends/explore?q=pi+day+2020&date=now+7-d&geo=US"
+                                    },
+                                    {
+                                        "query": "pi day deals",
+                                        "exploreLink": "/trends/explore?q=pi+day+deals&date=now+7-d&geo=US"
+                                    }
+                                ],
+                                "image": {
+                                    "newsUrl": "https://www.timesfreepress.com/news/life/entertainment/story/2020/mar/14/pi-day-specials-region/518189/",
+                                    "source": "Chattanooga Times Free Press",
+                                    "imageUrl": "https://t2.gstatic.com/images?q=tbn:ANd9GcQjP1Lf71vbIRdACcOx2pILmRmO2KFYQoNI35w6XZwjrQpyvbEF5w8UcHUB5jv2oaNkO6FWPxSS"
+                                },
+                                "articles": [
+                                    {
+                                        "title": "Check out these Pi Day specials in the Chattanooga region",
+                                        "timeAgo": "22h ago",
+                                        "source": "Chattanooga Times Free Press",
+                                        "image": {
+                                            "newsUrl": "https://www.timesfreepress.com/news/life/entertainment/story/2020/mar/14/pi-day-specials-region/518189/",
+                                            "source": "Chattanooga Times Free Press",
+                                            "imageUrl": "https://t2.gstatic.com/images?q=tbn:ANd9GcQjP1Lf71vbIRdACcOx2pILmRmO2KFYQoNI35w6XZwjrQpyvbEF5w8UcHUB5jv2oaNkO6FWPxSS"
+                                        },
+                                        "url": "https://www.timesfreepress.com/news/life/entertainment/story/2020/mar/14/pi-day-specials-region/518189/",
+                                        "snippet": "What could be better than a pie on Pi Day, March 14, or numerically 03/14? How about paying $3.14 for a pizza, or apple pie?"
+                                    },
+                                    {
+                                        "title": "16 Best Pi Day Deals to Take Advantage of on 3/14",
+                                        "timeAgo": "1d ago",
+                                        "source": "GoodHousekeeping.com",
+                                        "image": {
+                                            "newsUrl": "https://www.goodhousekeeping.com/life/money/a31404597/pi-day-deals-2020/",
+                                            "source": "GoodHousekeeping.com",
+                                            "imageUrl": "https://t2.gstatic.com/images?q=tbn:ANd9GcREfQBEIoXUbGqneiR0NJ_NrxSqP13MVluDkiQROIiClEcigbOaoptuqWiKPRoQofA_8Y0_jV3F"
+                                        },
+                                        "url": "https://www.goodhousekeeping.com/life/money/a31404597/pi-day-deals-2020/",
+                                        "snippet": "This year, Pi Day takes place on Saturday, March 14. Make sure to celebrate with these incredible Pi Day deals on all types of pies, both savory and sweet."
+                                    },
+                                    {
+                                        "title": "LIST | Pi Day Deals Around Maryland",
+                                        "timeAgo": "23h ago",
+                                        "source": "CBS Baltimore",
+                                        "url": "https://baltimore.cbslocal.com/2020/03/14/list-pi-day-deals-around-maryland/",
+                                        "snippet": "(CNN) — If you like to save money, then you&#39;ll love today — It&#39;s Pi Day. Pi, or 3.14159265 and so on, is the ratio of the circumference of a circle to its diameter."
+                                    },
+                                    {
+                                        "title": "Blaze Pizza reschedules Pi Day deal, extends through end of the year",
+                                        "timeAgo": "23h ago",
+                                        "source": "KLAS - 8 News Now",
+                                        "image": {
+                                            "newsUrl": "https://www.8newsnow.com/news/local-news/blaze-pizza-reschedules-pi-day-deal-extends-through-end-of-the-year/",
+                                            "source": "KLAS - 8 News Now",
+                                            "imageUrl": "https://t3.gstatic.com/images?q=tbn:ANd9GcTZkD9B1P15NFdvF9DWss2dAp2zJghqAPopwSHbgIOuOyMlX5-4CXsWYJtx5Z5YlNMlViqzuHXV"
+                                        },
+                                        "url": "https://www.8newsnow.com/news/local-news/blaze-pizza-reschedules-pi-day-deal-extends-through-end-of-the-year/",
+                                        "snippet": "LAS VEGAS (CNN/KLAS) — Saturday is the 14th day of the third month, which means March 14 is Pi Day! The day is meant to celebrate the mathematical&nbsp;..."
+                                    },
+                                    {
+                                        "title": "National Pi Day 2020 Deals: Discounts at Blaze Pizza, Papa Johns ...",
+                                        "timeAgo": "23h ago",
+                                        "source": "Newsweek",
+                                        "image": {
+                                            "newsUrl": "https://www.newsweek.com/national-pi-day-2020-deals-1492130",
+                                            "source": "Newsweek",
+                                            "imageUrl": "https://t2.gstatic.com/images?q=tbn:ANd9GcTLdUggFe2CucSgpWsWInjhCvcmFaq4OZ6ncMNs0qw5Lo69IiPUID4sxWYRYF4W4F2z3nU3YlqY"
+                                        },
+                                        "url": "https://www.newsweek.com/national-pi-day-2020-deals-1492130",
+                                        "snippet": "Today, March 14 (3/14), is National Pi Day, a day that celebrates the mathematical symbol we all came to know and love in school. Also known as π, the Pi is the&nbsp;..."
+                                    },
+                                    {
+                                        "title": "National Pi Day 2020 Deals &amp; Freebies",
+                                        "timeAgo": "1d ago",
+                                        "source": "Heavy.com",
+                                        "image": {
+                                            "newsUrl": "https://heavy.com/entertainment/2020/03/pi-day-2020-deals-freebies-specials/",
+                                            "source": "Heavy.com",
+                                            "imageUrl": "https://t2.gstatic.com/images?q=tbn:ANd9GcRbR1g-6B768Ke-JgbEEIxRPar9HLlkCOuH-5SjSyprFzTltL8fPB0cNa4UeFkrrPsMwh7wYXXe"
+                                        },
+                                        "url": "https://heavy.com/entertainment/2020/03/pi-day-2020-deals-freebies-specials/",
+                                        "snippet": "In honor of National Pi Day 2020 on March 14, many restaurants and fast-food chains are offering deals and discounts to customers."
+                                    },
+                                    {
+                                        "title": "Pi Day 2020 deals: Free pizza, free pie, and all the other top deals",
+                                        "timeAgo": "1d ago",
+                                        "source": "BGR",
+                                        "image": {
+                                            "newsUrl": "https://bgr.com/2020/03/14/pi-day-2020-deals-free-pizza-free-pie-best-deals/",
+                                            "source": "BGR",
+                                            "imageUrl": "https://t2.gstatic.com/images?q=tbn:ANd9GcRGV5QB5XLPw4WPZGr22EVnJHKPhyFRqUgFKowt0NbG1Fsl0Zw0cbubXGLCrjAZvVmcEdYzsNtH"
+                                        },
+                                        "url": "https://bgr.com/2020/03/14/pi-day-2020-deals-free-pizza-free-pie-best-deals/",
+                                        "snippet": "Pi Day 2020 is today, and there are tons of deals available from big restaurants and retailers across the country. All the best Pi Day deals that are available out&nbsp;..."
+                                    }
+                                ],
+                                "shareUrl": "https://trends.google.com/trends/trendingsearches/daily?geo=US&tt=Pi#Pi"
+                            }
+                        ]
+                    }
+                ],
+                "endDateForNextRequest": "20200313",
+                "rssFeedPageUrl": "https://trends.google.com/trends/trendingsearches/daily/rss?geo=US"
+            }
+        }"#;
+        let resp: DailyTrendsResponse =
+            serde_json::from_str(s).expect("failed to deserialize payload");
+        assert_eq!(
+            resp,
+            DailyTrendsResponse {
+                default: DailyTrendsData {
+                    trending_searches_days: vec![TrendingSearchDay {
+                        trending_searches: vec![
+                            TrendingSearch {
+                                title: TrendingSearchTitle {
+                                    query: "Coronavirus tips".into(),
+                                }
+                            },
+                            TrendingSearch {
+                                title: TrendingSearchTitle { query: "Pi".into() }
+                            }
+                        ],
+                    }]
+                }
+            }
+        );
     }
 }
